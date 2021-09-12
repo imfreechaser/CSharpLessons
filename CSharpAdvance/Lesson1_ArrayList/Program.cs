5b8e0e9fc1345df941ced73cb7494bb74d6a04f0
erina <520175084@qq.com>
2021-09-12 22:46:53 +0800

    new

---
+++ CSharpAdvance/Lesson1_ArrayList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson1_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        ArrayList array = new ArrayList();
+    }
+}
