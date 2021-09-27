0c1495a2ad3c705ce211a318c74b39b28c341d9f
erina <520175084@qq.com>
2021-09-27 22:03:17 +0800

    lesson7done

---
+++ CSharpAdvance/Lesson7_List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson7_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 10, 9,8,7,6,5,4,3,2,1 });
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            list.RemoveAt(4);
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
