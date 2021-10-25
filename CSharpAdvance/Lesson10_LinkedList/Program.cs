5f44c1bae7ad83e49adc5f03c5d3eb48069b1a6a
erina <520175084@qq.com>
2021-10-25 23:27:42 +0800

    lesson10done

--- CSharpAdvance/Lesson10_LinkedList/Program.cs
+++ CSharpAdvance/Lesson10_LinkedList/Program.cs
@@ -7,7 +7,25 @@ namespace Lesson10_LinkedList
     {
         static void Main(string[] args)
         {
-            
+            LinkedList<int> intList = new LinkedList<int>();
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                intList.AddFirst(r.Next(-100, 100));
+            }
+            LinkedListNode<int> tempNode = intList.First;
+            while (tempNode != null)
+            {
+                Console.WriteLine(tempNode.Value);
+                tempNode = tempNode.Next;
+            }
+            Console.WriteLine("**********************************");
+            tempNode = intList.Last;
+            while (tempNode != null)
+            {
+                Console.WriteLine(tempNode.Value);
+                tempNode = tempNode.Previous;
+            }
         }
     }
 }
