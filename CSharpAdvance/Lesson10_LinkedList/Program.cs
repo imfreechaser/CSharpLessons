f8a66d5d62dd92ea8aef30e4af5e38254ad4a87a
imfreechaser <520175084@qq.com>
2021-10-25 13:27:08 +0800

    lesson10done

---
+++ CSharpAdvance/Lesson10_LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson10_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
