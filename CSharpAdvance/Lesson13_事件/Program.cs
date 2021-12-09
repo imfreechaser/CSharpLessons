9e1a24c51286aa831e585871f10812cc949cd3df
erina <520175084@qq.com>
2021-12-09 23:51:45 +0800

    lesson29done

---
+++ CSharpAdvance/Lesson13_事件/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lesson13_事件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heater heater = new Heater();
+            Alertor alertor = new Alertor(heater);
+            Screen screen = new Screen(heater);
+
+            heater.TempOver95dgr(96f);
+
+        }
+    }
+    class Heater
+    {
+        public delegate void BoilingInformation(float waterTemp);
+
+        public event BoilingInformation boil;
+        
+        public void TempOver95dgr(float waterTemp)
+        {
+            if(boil != null && waterTemp > 95f)
+            {
+                boil.Invoke(waterTemp);
+            }
+        }
+    }
+    class Alertor
+    {
+        public Alertor(Heater heater)
+        {
+            heater.boil += AlertorWork;
+        }
+        public void AlertorWork(float waterTemp)
+        {
+            Console.WriteLine("\n语音播报：\n目前水的温度为：{0}度", waterTemp);
+        }
+    }
+    class Screen
+    {
+        public Screen(Heater heater)
+        {
+            heater.boil += ShowBoil;
+        }
+        public void ShowBoil(float waterTemp)
+        {
+            Console.WriteLine("\n显示器显示：\n水烧开了！");
+        }
+    }
+}
---
+++ CSharpAdvance/Lesson14_匿名函数/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson14_匿名函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            Console.WriteLine(t.func1(5));
+        }
+        
+  
+    }
+    class Test
+    {  
+        public static Func<int, int> Fun1(int a)
+        {
+            //改变了a的生命周期
+            return delegate (int b)
+            {
+                return a * b;
+            };
+        }
+        public Func<int, int> func1 = Fun1(5);
+    }
+
+}
