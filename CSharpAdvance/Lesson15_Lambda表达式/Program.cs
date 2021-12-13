1a76423841f20006337387945a714616e11fa8ae
imfreechaser <520175084@qq.com>
2021-12-13 15:10:32 +0800

    lesson32done

---
+++ CSharpAdvance/Lesson15_Lambda表达式/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson15_Lambda表达式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.SomeMethod()();
+        }
+    }
+    class Test
+    {
+
+        public Action SomeMethod()
+        {
+            Action action = null;
+            for (int i = 1; i < 11; i++)
+            {
+                int index = i;
+                action += () =>
+                {
+                    Console.WriteLine(index);
+                };
+            }
+            return action;
+        }
+    }
+}
