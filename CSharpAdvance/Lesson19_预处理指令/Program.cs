02fb6274637d4a5797bd86b1acc8eedaaed73422
imfreechaser <520175084@qq.com>
2021-12-22 17:35:39 +0800

    practice40done

---
+++ CSharpAdvance/Lesson19_预处理指令/Program.cs
@@ -0,0 +1,29 @@
+﻿//#define Unity5
+//#define Unity2017
+#undef Unity2020
+using System;
+
+namespace Lesson19_预处理指令
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountUnderDifVersion(1, 5));
+
+        }
+
+        static int CountUnderDifVersion(int a, int b)
+        {
+#if Unity5
+            return a + b;
+#elif Unity2017
+            return a * b;
+#elif Unity2020
+            return a - b;
+#else 
+            return 0;
+#endif
+        }
+    }
+}
